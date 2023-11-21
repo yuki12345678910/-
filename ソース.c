b5ce0ea3e068faa70e62e6fd92d984b758463bbe
yuki12345678910 <148539078+yuki12345678910@users.noreply.github.com>
2023-11-21 14:42:49 +0900

    Add files via upload

--- ソース.c
+++ ソース.c
@@ -1,10 +1,52 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <ctype.h>
 
-int main(void) {
-	for (int i = 1; i <= 10000; i++) {
-	  if ((i % 13 == 0) &&( i % 17 == 0)) {
-			printf("%d\n", i);
-		}
+//大文字　-> 小文字への変換関数
+char toLower(char c) {
+	if (isupper(c)) {
+		return tolower(c);
 	}
+	return c;
+}
+
+// 小文字 -> 大文字への変換関数
+char toUpper(char c) {
+	if (islower(c)) {
+		return toupper(c);
+	}
+	return c;
+}
+
+int main() {
+	char input[100];
+
+	printf("文字列を入力してください:");
+	fgets(input, sizeof(input), stdin);
+
+int option;
+	printf("処理のオプションを選択してください:\n1.すべての文字を小文字に変換\n2.すべての文字を大文字に変換\nオプション:");
+	scanf_s("%d", &option);
+
+	printf("\n変換後の文字列:");
+	int i = 0;
+	char convertedChar;
+	while (input[i] != '\0') {
+		switch (option) {
+		case 1:
+			convertedChar =
+				toLower(input[i]);
+			break;
+		case2:
+			convertedChar =
+				toUpper(input[i]);
+			break;
+		  default:
+			printf("無効なオプションが選択されました。\n");
+			return 0;
+		}
+		printf("%c", convertedChar);
+		i++;
+	}
+
 	return 0;
 }
